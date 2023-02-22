53cf4eb631834cd0cb80d0f90c34220b620c2c87
Khabib <k.khaysadykov@innopolis.university>
2023-02-22 15:18:26 +0300

    Add finished lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
@@ -17,12 +18,42 @@ enum TokenType : int {
     SEMICOLON,
     ASSIGN,
     PLUS,
+    PLUS_EQUAL,
     MINUS,
     MUL,
     DIV,
     LPAREN,
     RPAREN,
-    EOF_
+    LBRACKET,
+    RBRACKET,
+    LBRACE,
+    RBRACE,
+    EOF_,
+    READ_INT,
+    READ_REAL,
+    READ_STRING,
+    PRINT,
+    IF,
+    THEN,
+    END,
+    LESS, 
+    LESS_E,
+    GRATER,
+    GRATER_E,
+    EQUAL,
+    NOT_EQUAL, 
+    NOT,
+    WHILE_L,
+    FOR_L,
+    LOOP,
+    FUNC,
+    RETURN, 
+    DO, 
+    DOT_OP,
+    INVALID,
+    AND,
+    OR,
+    XOR,
 };
 
 const string TokenTypeStr[] = {
@@ -38,185 +69,299 @@ const string TokenTypeStr[] = {
     [TokenType::SEMICOLON] = "SEMICOLON",
     [TokenType::ASSIGN] = "ASSIGN",
     [TokenType::PLUS] = "PLUS",
+    [TokenType::PLUS_EQUAL] = "PLUS_EQUAL",
     [TokenType::MINUS] = "MINUS",
     [TokenType::MUL] = "MUL",
     [TokenType::DIV] = "DIV",
     [TokenType::LPAREN] = "LPAREN",
     [TokenType::RPAREN] = "RPAREN",
+    [TokenType::LBRACKET] = "LBRACKET",
+    [TokenType::RBRACKET] = "RBRACKET",
+    [TokenType::LBRACE] = "LBRACE",
+    [TokenType::RBRACE] = "RBRACE",
     [TokenType::EOF_] = "EOF_",
+    [TokenType::READ_INT] = "READ_INT",
+    [TokenType::READ_REAL] = "READ_REAL",
+    [TokenType::READ_STRING] = "READ_STRING",
+    [TokenType::PRINT] = "PRINT",
+    [TokenType::IF] = "IF",
+    [TokenType::THEN] = "THEN",
+    [TokenType::END] = "END",
+    [TokenType::LESS] = "LESS",
+    [TokenType::LESS_E] = "LESS_E",
+    [TokenType::GRATER] = "GRATER",
+    [TokenType::GRATER_E] = "GRATER_E",
+    [TokenType::EQUAL] = "EQUAL",
+    [TokenType::NOT_EQUAL] = "NOT_EQUAL",
+    [TokenType::NOT] = "NOT",
+    [TokenType::WHILE_L] = "WHILE_L",
+    [TokenType::FOR_L] = "FOR_L",
+    [TokenType::LOOP] = "LOOP",
+    [TokenType::FUNC] = "FUNC",
+    [TokenType::RETURN] = "RETURN",
+    [TokenType::DO] = "DO",
+    [TokenType::DOT_OP] = "DOT_OP",
+    [TokenType::INVALID] = "INVALID",
+    [TokenType::AND] = "AND",
+    [TokenType::OR] = "OR",
+    [TokenType::XOR] = "XOR",
 };
 
 class Token {
-public:
-    Token(TokenType type, string lexeme, int line) : type(type), lexeme(lexeme), line(line) {}
+    public:
+        Token(TokenType type, string lexeme, size_t line) : type(type), lexeme(lexeme), line(line) {}
 
-    TokenType getType() const { return type; }
+        Token(TokenType type, string lexeme) : type(type), lexeme(lexeme) {}
 
-    string getLexeme() const { return lexeme; }
+        TokenType getType() const { return type; }
 
-private:
-    TokenType type;
-    string lexeme;
-    int line;
+        string getLexeme() const { return lexeme; }
+
+        size_t getLine() const { return line; }
+
+    private:
+        TokenType type;
+        string lexeme;
+        size_t line;
 };
 
 class Lexer {
-public:
-    Lexer(string input) : input(input) {}
+    public:
+        Lexer(string input) : input(input) {}
 
-    vector<Token> tokenize() {
-        vector<Token> tokens;
-        size_t i = 0;
-        size_t curentLine = 0;
+        vector<Token> tokenize() {
+            vector<Token> tokens;
+            size_t i = 0;
+            size_t curentLine = 0;
+            regex pattern("[a-zA-Z_]");
 
-        while (i < input.length()) {
-            char c = input[i];
+            while (i < input.length()) {
+                char c = input[i];
 
-            if (c == ' ' || c == '\n' || c == '\r' || c == '\t') {
-                if(c == '\n') curentLine++;
-                i++;
-            } else if (isalpha(c)) {
-                string identifier = readIdentifier(i);
-                TokenType type = getType(identifier);
-                tokens.emplace_back(type, identifier, curentLine);
-            } else if (isdigit(c)) {
-                string number = readNumber(i);
-                tokens.emplace_back(TokenType::NUMBER, number, curentLine);
-            } else if (c == '"') {
-                string str = readString(i);
-                tokens.emplace_back(TokenType::STRING, str, curentLine);
-            } else if (c == '#') {
-                skipComment(i);
-            } else if (c == ',') {
-                tokens.emplace_back(TokenType::COMMA, ",", curentLine);
-                i++;
-            } else if (c == ';') {
-                tokens.emplace_back(TokenType::SEMICOLON, ";", curentLine);
-                i++;
-            } else if (c == '=') {
-                if (input[i + 1] == '=') {
-                    tokens.emplace_back(TokenType::IS, "==", curentLine);
-                    i += 2;
+                if (c == ' ' || c == '\n' || c == '\r' || c == '\t') {
+                    if(c == '\n') curentLine++;
+                    i++;
+                } else if (regex_match(string(1, c), pattern)) {
+                    string identifier = readIdentifier(i);
+                    TokenType type = getType(identifier);
+                    tokens.emplace_back(type, identifier, curentLine);
+                } else if (isdigit(c)) {
+                    string number = readNumber(i);
+                    tokens.emplace_back(TokenType::NUMBER, number, curentLine);
+                } else if (c == '"') {
+                    string str = readString(i);
+                    tokens.emplace_back(TokenType::STRING, str, curentLine);
+                } else if (c == '#') {
+                    skipComment(i, curentLine);
+                } else if (c == ',') {
+                    tokens.emplace_back(TokenType::COMMA, ",", curentLine);
+                    i++;
+                } else if (c == ';') {
+                    tokens.emplace_back(TokenType::SEMICOLON, ";", curentLine);
+                    i++;
+                } else if (c == '=') {
+                    if (input[i + 1] == '=') {
+                        tokens.emplace_back(TokenType::EQUAL, "==", curentLine);
+                        i += 2;
+                    } else {
+                        tokens.emplace_back(TokenType::ASSIGN, "=", curentLine);
+                        i++;
+                    }
+                } else if (c == '<') {
+                    if (input[i + 1] == '=') {
+                        tokens.emplace_back(TokenType::LESS_E, "<=", curentLine);
+                        i += 2;
+                    } else {
+                        tokens.emplace_back(TokenType::LESS, "<", curentLine);
+                        i++;
+                    }
+                } else if (c == '>') {
+                    if (input[i + 1] == '=') {
+                        tokens.emplace_back(TokenType::GRATER_E, ">=", curentLine);
+                        i += 2;
+                    } else {
+                        tokens.emplace_back(TokenType::GRATER, ">", curentLine);
+                        i++;
+                    }
+                } else if (c == '!') {
+                    if (input[i + 1] == '=') {
+                        tokens.emplace_back(TokenType::NOT_EQUAL, "!=", curentLine);
+                        i += 2;
+                    } else {
+                        tokens.emplace_back(TokenType::NOT, "!", curentLine);
+                        i++;
+                    }
+                } else if (c == '+') {
+                    if (input[i + 1] == '=') {
+                        tokens.emplace_back(TokenType::PLUS_EQUAL, "+=", curentLine);
+                        i += 2;
+                    } else{
+                        tokens.emplace_back(TokenType::PLUS, "+", curentLine);
+                        i++;
+                    }
+                } else if (c == '-') {
+                    tokens.emplace_back(TokenType::MINUS, "-", curentLine);
+                    i++;
+                } else if (c == '*') {
+                    tokens.emplace_back(TokenType::MUL, "*", curentLine);
+                    i++;
+                } else if (c == '/') {
+                    tokens.emplace_back(TokenType::DIV, "/", curentLine);
+                    i++;
+                } else if (c == '(') {
+                    tokens.emplace_back(TokenType::LPAREN, "(", curentLine);
+                    i++;
+                } else if (c == ')') {
+                    tokens.emplace_back(TokenType::RPAREN, ")", curentLine);
+                    i++;
+                } else if (c == '[') {
+                    tokens.emplace_back(TokenType::LBRACKET, "[", curentLine);
+                    i++;
+                } else if (c == ']') {
+                    tokens.emplace_back(TokenType::RBRACKET, "]", curentLine);
+                    i++;
+                } else if (c == '{') {
+                    tokens.emplace_back(TokenType::LBRACE, "{", curentLine);
+                    i++;
+                } else if (c == '}') {
+                    tokens.emplace_back(TokenType::RBRACE, "}", curentLine);
+                    i++;
+                } else if (c == '.') {
+                    tokens.emplace_back(TokenType::DOT_OP, ".", curentLine);
+                    i++;
                 } else {
-                    tokens.emplace_back(TokenType::ASSIGN, "=", curentLine);
+                    tokens.emplace_back(TokenType::INVALID, string(1, c), curentLine);
                     i++;
                 }
-            } else if (c == '+') {
-                tokens.emplace_back(TokenType::PLUS, "+", curentLine);
+            }
+
+            tokens.emplace_back(TokenType::EOF_, "", curentLine);
+            return tokens;
+        }
+
+    private:
+        string input;
+
+        string readIdentifier(size_t& i) {
+            size_t start = i;
+            regex pattern("[a-zA-Z_]");
+            
+            while (i < input.length() && regex_match(string(1, input[i]), pattern)) {
                 i++;
-            } else if (c == '-') {
-                tokens.emplace_back(TokenType::MINUS, "-", curentLine);
+            }
+            return input.substr(start, i - start);
+        }
+
+        string readNumber(size_t& i) {
+            size_t start = i;
+            bool isDotAppear = false;
+            while (i < input.length() && (isdigit(input[i]) || input[i] == '.')) {
+                if(input[i] == '.') {
+                    isDotAppear = true;
+                    i++;
+                } else if(input[i] == '.' && isDotAppear) break;
                 i++;
-            } else if (c == '*') {
-                tokens.emplace_back(TokenType::MUL, "*", curentLine);
+            }
+            return input.substr(start, i - start);
+        }
+
+        string readString(size_t& i) {
+            size_t start = ++i;
+            while (i < input.length()) {
+                if (input[i] == '\\') {
+                    i += 2; // skip the escape sequence
+                } else if (input[i] == '"') {
+                    i++;
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            return input.substr(start, i - start - 1);
+        }
+
+
+        void skipComment(size_t& i, size_t& line){
+            while (i < input.length()) {
+                if (input[i] == '\n') {
+                    line++;
+                    i++;
+                    break;
+                }
                 i++;
-            } else if (c == '/') {
-                tokens.emplace_back(TokenType::DIV, "/", curentLine);
-                i++;
-            } else if (c == '(') {
-                tokens.emplace_back(TokenType::LPAREN, "(", curentLine);
-                i++;
-            } else if (c == ')') {
-                tokens.emplace_back(TokenType::RPAREN, ")", curentLine);
-                i++;
-            } else {
-                cerr << "Invalid character: " << c << endl;
-                exit(1);
             }
         }
 
-        tokens.emplace_back(TokenType::EOF_, "", curentLine);
-        return tokens;
-    }
-
-private:
-    string input;
-
-    string readIdentifier(size_t& i) {
-        size_t start = i;
-        while (i < input.length() && (isalnum(input[i]) || input[i] == '_')) {
-            i++;
+    TokenType getType(string identifier) {
+        if (identifier == "var") {
+            return TokenType::VAR;
+        } else if (identifier == "const") {
+            return TokenType::CONST;
+        } else if (identifier == "null") {
+            return TokenType::NULL_;
+        } else if (identifier == "true" || identifier == "false") {
+            return TokenType::BOOL;
+        } else if (identifier == "is") {
+            return TokenType::IS;
+        } else if (identifier == "readInt") {
+            return TokenType::READ_INT;
+        } else if (identifier == "readReal") {
+            return TokenType::READ_REAL;
+        } else if (identifier == "readString") {
+            return TokenType::READ_STRING;
+        } else if (identifier == "print") {
+            return TokenType::PRINT;
+        } else if (identifier == "if") {
+            return TokenType::IF;
+        } else if (identifier == "then") {
+            return TokenType::THEN;
+        } else if (identifier == "end") {
+            return TokenType::END;
+        } else if (identifier == "print") {
+            return TokenType::PRINT;
+        } else if (identifier == "while") {
+            return TokenType::WHILE_L;
+        } else if (identifier == "for") {
+            return TokenType::FOR_L;
+        } else if (identifier == "loop") {
+            return TokenType::LOOP;
+        } else if (identifier == "func") {
+            return TokenType::FUNC;
+        } else if (identifier == "return") {
+            return TokenType::RETURN;
+        } else if (identifier == "do") {
+            return TokenType::DO;
+        } else if (identifier == "and") {
+            return TokenType::AND;
+        } else if (identifier == "or") {
+            return TokenType::OR;
+        } else if (identifier == "xor") {
+            return TokenType::XOR;
+        } else {
+            return TokenType::IDENTIFIER;
         }
-        return input.substr(start, i - start);
     }
-
-    string readNumber(size_t& i) {
-        size_t start = i;
-        while (i < input.length() && (isdigit(input[i]) || input[i] == '.')) {
-            i++;
-        }
-        return input.substr(start, i - start);
-    }
-
-    string readString(size_t& i) {
-        size_t start = ++i;
-        while (i < input.length()) {
-            if (input[i] == '\\') {
-                i += 2; // skip the escape sequence
-            } else if (input[i] == '"') {
-                i++;
-                break;
-            } else {
-                i++;
-            }
-        }
-        return input.substr(start, i - start - 1);
-    }
-
-
-    void skipComment(size_t& i){
-        while (i < input.length()) {
-            if (input[i] == '\n') {
-                i++;
-                break;
-            }
-            i++;
-        }
-    }
-
-
-TokenType getType(string identifier) {
-    if (identifier == "var") {
-        return TokenType::VAR;
-    } else if (identifier == "const") {
-        return TokenType::CONST;
-    } else if (identifier == "null") {
-        return TokenType::NULL_;
-    } else if (identifier == "true" || identifier == "false") {
-        return TokenType::BOOL;
-    } else {
-        return TokenType::IDENTIFIER;
-    }
-}
 
 };
 
 int main() {
 string input = R"(
 # btw, this is comment ;)
-
 # create mutable variable
 var x = 1.23;
-
 # reassign mutable variable to another type
 x = "Hello"; # OK
-
 # assign multiple variables in one line
 var a = 30, b = false;
-
 # can create variable via expression
 var g = 10, h = 20;
 var j = g * h / 2;
-
 # initialize empty variable (it has special type `null`)
 var c; # c is null
-
 # create literal variable 
 const y = "This is string";
-
 # can check type of variable
 var isYString = (y is string); # isYString = true
-
 # can check for null
 var isYNull = (y is null); # isYNull = false)";
 
