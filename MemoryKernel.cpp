9b422c6f15d336c8ae38b6cee23ca79d71ba4886
Emil Latypov <emillatypov9335@gmail.com>
2023-04-29 11:01:23 +0300

    memory: Implement basic methods
    It is not testes, not ready for usage yet

--- MemoryKernel.cpp
+++ MemoryKernel.cpp
@@ -1,1 +1,174 @@
 #include "MemoryKernel.hpp"
+
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+#include "ast.hpp"
+
+/**************************************************
+ *           Local Functions Prototypes
+ **************************************************/
+
+/**
+ * @brief Checks if name satisfies array element pattern
+ *        (ARRAY_NAME@ARRAY_ELEMENT)
+ *
+ * @param name Name of element to check
+ * @return true If matches array element pattern
+ * @return false If does not match array element pattern
+ */
+static bool is_array_element(std::string name);
+
+/**
+ * @brief Extract array name from pattern
+ *        (e.g. from arr@123 extracts arr)
+ *
+ * Warning: `is_array_element` should be called first
+ *
+ * @param name Name of element to be extracted
+ * @return extracted name of array
+ */
+static std::string extract_array_name(std::string name);
+
+/**
+ * @brief Extract array as list from memory
+ *
+ * @param name Name of array to be extracted
+ * @return vector of pairs, where key is name of element
+ *         without array prefix and value is pointer to MemObject
+ */
+static std::vector<std::pair<std::string, MemObject *>> extract_array(
+    std::string name);
+
+/**************************************************
+ *           MemObject Implementation
+ **************************************************/
+
+MemObject::MemObject(ObjectType type, std::string name, std::string value)
+    : type(type), name(name), value(value), num_references(0){};
+
+MemObject::~MemObject() {
+  if (!count_references()) {
+    // TODO: implement warning
+  }
+}
+
+ObjectType MemObject::get_type() const { return this->type; }
+
+std::string MemObject::get_name() const { return this->name; }
+
+std::string MemObject::get_value() const { return this->value; }
+
+unsigned int MemObject::count_references() const {
+  return this->num_references;
+}
+
+void MemObject::set_type(ObjectType type) { this->type = type; }
+
+void MemObject::set_value(std::string value) { this->value = value; }
+
+void MemObject::ref_inc() { this->num_references++; }
+
+/**************************************************
+ *          MemFunction Implementation
+ **************************************************/
+
+MemFunction::MemFunction(std::string name, AST::Block *entry_point,
+                         std::vector<std::string> arg_names)
+    : MemObject(OBJECT_FUNC, name, "(func)"),
+      entry_point(entry_point),
+      arg_names(arg_names) {}
+
+AST::Block *MemFunction::get_entry_point() const { return this->entry_point; }
+
+unsigned int MemFunction::count_args() { return this->arg_names.size(); }
+
+bool MemFunction::prep_mem(MemoryKernel &mem, std::vector<MemObject> args) {
+  // TODO: prepare memory before function call
+  return false;
+}
+
+/**************************************************
+ *           MemoryKernel Implementation
+ **************************************************/
+
+MemoryKernel::MemoryKernel() {
+  this->scopes = std::vector<std::vector<MemObject *>>();
+}
+
+MemObject *MemoryKernel::get_object(std::string name) const {
+  for (auto &scope : this->scopes) {
+    for (MemObject *object : scope) {
+      if (object->get_name() == name) return object;
+    }
+  }
+
+  return nullptr;
+}
+
+bool MemoryKernel::put_object(std::string name, MemObject *obj) {
+  // TODO: Implement
+  return false;
+}
+
+bool MemoryKernel::drop_object(std::string name) {
+  MemObject *obj = get_object(name);
+  if (!obj) return false;
+
+  for (auto &scope : this->scopes) {
+    for (int i = 0; i < scope.size(); ++i) {
+      if (scope[i] == obj) scope.erase(scope.begin() + i);
+    }
+  }
+
+  return true;
+}
+
+void MemoryKernel::enter_scope() {
+  scopes.push_back(std::vector<MemObject *>());
+}
+
+void MemoryKernel::exit_scope() { scopes.pop_back(); }
+
+void MemoryKernel::dump_mem() const {
+  std::cout << "{\n";
+  int depth = 1;
+  for (auto &scope : this->scopes) {
+    for (MemObject *obj : scope) {
+      for (int i = 0; i < depth; ++i) std::cout << " ";
+      std::cout << obj->get_name() << " = " << obj->get_value() << "\n";
+      ++depth;
+    }
+  }
+  std::cout << "}\n";
+}
+
+/**************************************************
+ *         Local Functions Implementation
+ **************************************************/
+
+static bool is_array_element(std::string name) {
+  static std::regex pattern("..*@..*");
+  return std::regex_match(name, pattern);
+}
+
+static std::string extract_array_name(std::string name) {
+  std::stringstream ss;
+
+  for (auto &c : name) {
+    if (c == '@')
+      return ss.str();
+    else
+      ss << c;
+  }
+
+  return "";
+}
+
+static std::vector<std::pair<std::string, MemObject *>> extract_array(
+    std::string name) {
+  // TODO: Implement
+  return std::vector<std::pair<std::string, MemObject *>>();
+}
--- MemoryKernel.hpp
+++ MemoryKernel.hpp
@@ -17,12 +17,11 @@ class MemoryKernel;
  *
  * (arrays and tuples are processed separately)
  */
-enum ObjectType : int
-{
-    OBJECT_STRING = 0,
-    OBJECT_NUMBER,
-    OBJECT_BOOL,
-    OBJECT_FUNC,
+enum ObjectType : int {
+  OBJECT_STRING = 0,
+  OBJECT_NUMBER,
+  OBJECT_BOOL,
+  OBJECT_FUNC,
 };
 
 /**
@@ -30,35 +29,34 @@ enum ObjectType : int
  * (can be changed if needed, MemoryKernel operates only with pointers)
  *
  */
-class MemObject
-{
-private:
-    ObjectType type;
-    std::string name;
-    std::string value;
+class MemObject {
+ private:
+  ObjectType type;
+  std::string name;
+  std::string value;
 
-    // number of times object was used by other objects
-    unsigned int num_references;
+  // number of times object was used by other objects
+  unsigned int num_references;
 
-public:
-    MemObject(ObjectType type, std::string name, std::string value);
+ public:
+  MemObject(ObjectType type, std::string name, std::string value);
 
-    // show warning if object was not used
-    ~MemObject();
+  // show warning if object was not used
+  ~MemObject();
 
-    // getters
-    ObjectType get_type() const;
-    std::string get_name() const;
-    std::string get_value() const;
+  // getters
+  ObjectType get_type() const;
+  std::string get_name() const;
+  std::string get_value() const;
+  unsigned int count_references() const;
 
-    // setters
-    void set_type(ObjectType type);
-    void set_value(std::string value);
-    unsigned int count_references() const;
+  // setters
+  void set_type(ObjectType type);
+  void set_value(std::string value);
 
-    // increment number of references
-    // (needed to show warning if object was not used)
-    void ref_inc();
+  // increment number of references
+  // (needed to show warning if object was not used)
+  void ref_inc();
 };
 
 /**
@@ -66,37 +64,37 @@ public:
  * useful metainformation about function object
  *
  */
-class MemFunction : MemObject
-{
-private:
-    // entry point for function
-    AST::Block *entry_point;
+class MemFunction : MemObject {
+ private:
+  // entry point for function
+  AST::Block *entry_point;
 
-    // names of arguments required by function
-    // (required to prepare memory before function call)
-    std::vector<std::string> arg_names;
+  // names of arguments required by function
+  // (required to prepare memory before function call)
+  std::vector<std::string> arg_names;
 
-public:
-    MemFunction(AST::Block *entry_point, std::vector<std::string> arg_names);
+ public:
+  MemFunction(std::string name, AST::Block *entry_point,
+              std::vector<std::string> arg_names);
 
-    // Get entry point block for execution
-    // (do not forget to call `prep_mem` before run!)
-    AST::Block *get_entry_point() const;
+  // Get entry point block for execution
+  // (do not forget to call `prep_mem` before run!)
+  AST::Block *get_entry_point() const;
 
-    // Get number of arguments required by function
-    unsigned int count_args();
+  // Get number of arguments required by function
+  unsigned int count_args();
 
-    /**
-     * @brief Prepare memory before function call
-     * (correctly place args in memory,
-     *  even arrays and tuples processed)
-     *
-     * @param mem Reference to memory to be prepared
-     * @param args list of args
-     * @return true if memory prepared successfully
-     * @return false if failed to prepare memory (invalid number of args)
-     */
-    bool prep_mem(MemoryKernel &mem, std::vector<MemObject> args);
+  /**
+   * @brief Prepare memory before function call
+   * (correctly place args in memory,
+   *  even arrays and tuples processed)
+   *
+   * @param mem Reference to memory to be prepared
+   * @param args list of args
+   * @return true if memory prepared successfully
+   * @return false if failed to prepare memory (invalid number of args)
+   */
+  bool prep_mem(MemoryKernel &mem, std::vector<MemObject> args);
 };
 
 /**
@@ -117,47 +115,65 @@ public:
  *      additional actions performed to array/tuple management)
  *
  */
-class MemoryKernel
-{
-private:
-    // each element of scopes is a vector of objects in current scope
-    std::vector<std::vector<MemObject>> scopes;
+class MemoryKernel {
+ private:
+  // each element of scopes is a vector of objects in current scope
+  std::vector<std::vector<MemObject *>> scopes;
 
-public:
-    /**
-     * @brief Get the object by name
-     *
-     * (if array or tuple element is required,
-     *  then use ARRAY_NAME@ELEMENT_NAME notation)
-     *
-     * @param name Objects name
-     * @return Pointer to object or nullptr if it does not exist
-     */
-    MemObject *get_object(std::string name);
+ public:
+  MemoryKernel();
 
-    /**
-     * @brief Put object to memory
-     *
-     * @param name Object name
-     * @param obj Object itself
-     * @return true if object existed before
-     * @return false if object did not exist before
-     */
-    bool put_object(std::string name, MemObject *obj);
+  /**
+   * @brief Get the object by name
+   *
+   * (if array or tuple element is required,
+   *  then use ARRAY_NAME@ELEMENT_NAME notation)
+   *
+   * @param name Objects name
+   * @return Pointer to object or nullptr if it does not exist
+   */
+  MemObject *get_object(std::string name) const;
 
-    /**
-     * @brief Should be called on each new visibility scope entered
-     * (It is needed to track memory for each visibility scope
-     * and cleanup on scope exit)
-     */
-    void enter_scope();
+  /**
+   * @brief Put object to memory
+   *
+   * @param name Object name
+   * @param obj Object itself
+   * @return true if object existed before
+   * @return false if object did not exist before
+   */
+  bool put_object(std::string name, MemObject *obj);
 
-    /**
-     * @brief Should be called on each visibility scope exited
-     * (It is needed to track memory for each visibility scope
-     * and cleanup on scope exit)
-     */
-    void exit_scope();
+  /**
+   * @brief Manually delete object from memory
+   *        (May be useful on inline array redefinition)
+   *
+   * @param name Name of object to be dropped
+   * @return true If object dropped successfully 
+   * @return false If object does not exist
+   */
+  bool drop_object(std::string name);
+
+  /**
+   * @brief Should be called on each new visibility scope entered
+   * (It is needed to track memory for each visibility scope
+   * and cleanup on scope exit)
+   */
+  void enter_scope();
+
+  /**
+   * @brief Should be called on each visibility scope exited
+   * (It is needed to track memory for each visibility scope
+   * and cleanup on scope exit)
+   */
+  void exit_scope();
+
+  /**
+   * @brief Dump current memory to standard output
+   *        (Can be used in debug purposes)
+   * 
+   */
+  void dump_mem() const;
 };
 
-#endif // MEMORY_KERNEL_HPP
+#endif  // MEMORY_KERNEL_HPP
